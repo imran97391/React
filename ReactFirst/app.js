21f6c016408da677d69880b1bbf6115f860182b4
Imran <imali@deloitte.com>
2025-02-24 21:45:43 +0530

    added first lecture

--- ReactFirst/app.js
+++ ReactFirst/app.js
@@ -1,7 +1,23 @@
-const heading = React.createElement('div', {id:'parent'},React.createElement('div',{id:'child'},[React.createElement('h1',{},"heading 1"),React.createElement('h1',{},"heading 2")]) );
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+// core way to write react element
+// const heading = React.createElement('div', {id:'parent'},React.createElement('div',{id:'child'},[React.createElement('h1',{},"heading 1"),React.createElement('h1',{},"heading 2")]) );
+
+//Jsx element
+// const jsxHeading=<h1>jsxHeading</h1>
+
+const Title=()=>{
+    return <h1>Component Composition</h1>
+}
+const HeadingComponent=()=>{
+return <div>
+    <Title/>
+<h1>jsxHeadingHeadingComponent</h1>
+</div> 
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(heading);
+// console.log(jsxHeading);
 console.log(root);
 
-root.render(heading);
+root.render(<HeadingComponent/>);
